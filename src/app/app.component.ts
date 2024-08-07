47dd2924fce3b7979057a5d249e58bc14c7fdbf8
tristanshaw000 <tristanshaw000@gmail.com>
2024-08-07 19:37:23 +0000

    why

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {NgOptimizedImage} from "@angular/common";
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet, NgOptimizedImage],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'Countdown';
-  countdown: string = '';
+  title = "Countdown";
+  countdown: string = "Timer not started!";
   countdownInterval: any;
 
   async setNotification(inputValue: string) {
     const inputDateTime = new Date(inputValue);
-    console.log(inputDateTime);
 
     const permission = await requestNotificationPermission();
     if (permission === "granted" && !isNaN(inputDateTime.getTime())) {
       this.startCountdown(inputDateTime);
     } else {
-      alert("Notification permission was not granted or an error occurred while parsing the input date!");
+      alert(
+        "Notification permission was not granted or an error occurred while parsing the input date!"
+      );
     }
   }
 
@@ -30,7 +31,9 @@ export class AppComponent {
     if (Notification.permission === "granted") {
       new Notification(title, options);
     } else {
-      alert("Notification permission not granted which are required to use countdown!");
+      alert(
+        "Notification permission not granted which are required to use countdown!"
+      );
     }
   }
 
@@ -41,11 +44,15 @@ export class AppComponent {
 
       if (timeDiff <= 0) {
         this.countdown = "Event has started!";
-        this.deliverNotification("Countdown", "Countdown for event has ended!").then(_ => {});
+        this.deliverNotification(
+          "Countdown",
+          "Countdown for event has ended!"
+        ).then((_) => {});
         clearInterval(this.countdownInterval);
       } else {
         const seconds = Math.floor((timeDiff / 1000) % 60);
         const minutes = Math.floor((timeDiff / 1000 / 60) % 60);
+
         const hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
         const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 
@@ -60,7 +67,9 @@ export class AppComponent {
     if (permission === "granted") {
       this.showNotification(title, { body: body });
     } else {
-      alert("Notification permission not granted which are required to use countdown!");
+      alert(
+        "Notification permission not granted which are required to use countdown!"
+      );
     }
   }
 }
